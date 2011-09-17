f52d1decba024f1cc6bacf3528ec4ea81d07aa95
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2011-09-17 12:55:41 +0200

    [clone_scanner] Simplified buffer search.

--- clone_scanner.py
+++ clone_scanner.py
@@ -115,7 +115,7 @@ def on_join_scan_cb(data, signal, signal_data):
   parsed_host = join_match_data.group(2).lower()
   chan_name = join_match_data.group(3)
   network_chan_name = "%s.%s" % (network, chan_name)
-  chan_buffer = weechat.buffer_search("irc", "%s.%s" % (network, chan_name))
+  chan_buffer = weechat.buffer_search("irc", network_chan_name)
   if not chan_buffer:
     print "No IRC channel buffer found for %s" % network_chan_name
     return weechat.WEECHAT_RC_OK
